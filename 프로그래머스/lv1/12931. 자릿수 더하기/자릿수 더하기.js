220e58f0a33ca5a2d6852f1685d9e69baae9752a
zzzRYT <117448747+zzzRYT@users.noreply.github.com>
2023-05-31 17:18:17 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,5 +1,5 @@
 function solution(n) {
-    let arr = String(n).split('');
+    let arr = (n+"").split('');
     let tot = 0;
     for (let i = 0; i < arr.length; i++) {
         tot = tot + Number(arr[i]);
