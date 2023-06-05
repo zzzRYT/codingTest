8ca1312d0e1831b7df44b041f732240b9d07fedd
zzzRYT <117448747+zzzRYT@users.noreply.github.com>
2023-06-05 10:48:23 +0900

    [level 1] Title: 음양 더하기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,15 +1,38 @@
+// function solution(absolutes, signs) {
+//     let arr = [];
+//     let total = 0;
+//     for (let i = 0; i < absolutes.length; i++){
+//         if (signs[i] === true) {
+//             arr.push(absolutes[i]);
+//         } else {
+//             arr.push(((-2)*absolutes[i]) + absolutes[i]);
+//         }
+//     }
+//     for (let i = 0; i < arr.length; i++) {
+//         total = total + arr[i];
+//     }
+//     return total;
+// }
+
 function solution(absolutes, signs) {
-    let arr = [];
-    let total = 0;
-    for (let i = 0; i < absolutes.length; i++){
-        if (signs[i] === true) {
-            arr.push(absolutes[i]);
-        } else {
-            arr.push(((-2)*absolutes[i]) + absolutes[i]);
+    const pm = absolutes.reduce((acc, cur, idx) => {
+        if(signs[idx] === false) {
+            return acc + (cur * (-1));
+        } else if (signs[idx] === true) {
+            return acc + (cur * 1);
         }
-    }
-    for (let i = 0; i < arr.length; i++) {
-        total = total + arr[i];
-    }
-    return total;
+        // console.log(acc);
+    }, 0);
+    return pm;
 }
+    // const result = absolutes.reduce((acc, cur, idx) => {
+    //       if(cur*pm[idx] < 0){
+    //         // 처리할 현재 요소가 음수일 경우
+    //         acc.push(cur*arr[idx]);
+    //       }
+    //       else if(cur*pm[idx] > 0){
+    //         // 처리할 현재 요소가 양수일 경우
+    //         acc.push(cur*arr[idx]);
+    //       }
+    //       return acc;
+    // });
