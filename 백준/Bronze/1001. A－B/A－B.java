1e35b043ae9dd3386ccc3f92be17db6397bcf7c0
zzzRYT <jinjinstar3@gmail.com>
2024-05-05 22:48:00 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner num = new Scanner(System.in);
+        
+        int num1 = num.nextInt();
+        int num2 = num.nextInt();
+        
+        System.out.println(num1 - num2);
+    }
+}
