9b4cdc2d38ab23083656809fae577fa9164c6fb5
zzzRYT <jinjinstar3@gmail.com>
2024-05-16 15:55:43 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/README.md
+++ 프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994#) 
 
 ### 성능 요약
 
-메모리: 70.4 MB, 시간: 0.17 ms
+메모리: 73.6 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 16일 15:02:42
+2024년 05월 16일 15:55:41
 
 ### 문제 설명
 
--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,32 +1,18 @@
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Arrays;
-
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "";
-        String[] result = new String[goal.length];
-        Queue<String> cq1 = new LinkedList<>();
-        Queue<String> cq2 = new LinkedList<>();
-        
-        for(String ele: cards1) {
-            cq1.add(ele);
-        }
-        for(String ele: cards2) {
-            cq2.add(ele);
-        }
-        for(int i = 0; i < goal.length; i++) {
-            if(goal[i].equals(cq1.peek())) {
-                result[i] = cq1.remove(); 
-            } 
-            if(goal[i].equals(cq2.peek())){
-                result[i] = cq2.remove();
+        String answer = "Yes";
+        int j = 0;
+        int k = 0;
+
+        for (int i = 0; i < goal.length; i++) {
+            if (j < cards1.length && goal[i].equals(cards1[j])) {
+                j++;
+            } else if (k < cards2.length && goal[i].equals(cards2[k])) {
+                k++;
+            } else {
+                answer = "No";
+                return answer;
             }
-        }
-        if(Arrays.equals(result, goal)) {
-            answer = "Yes";
-        } else {
-            answer = "No";
         }
         return answer;
     }
