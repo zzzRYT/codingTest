b7963236ea5da2e7409cc6ba49d9f3330bb344c5
zzzRYT <jinjinstar3@gmail.com>
2024-06-03 19:33:35 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,13 +1,8 @@
 function solution(num) {
     let count = 0;
     while(num > 1) {
-        if(num % 2 === 0) {
-            num = num / 2;
-            count++;
-        }else {
-            num = (num * 3) + 1;
-            count++;
-        }
+        num % 2 === 0 ? num = num / 2 : num=(num*3)+1;
+        count++;
         if(count >=500) return -1;
     }
     return count;
