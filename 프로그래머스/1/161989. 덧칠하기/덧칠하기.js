1ed309bd6e683c7752c5983338b6f617273e4b2d
zzzRYT <jinjinstar3@gmail.com>
2024-09-01 15:59:45 +0900

    [level 1] Title: 덧칠하기, Time: 6.24 ms, Memory: 40.4 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,33 +1,23 @@
-function solution(n, m, section) {
-    //1m 가 한 구역인 n개
-    //m미터의 롤러
-    //section 칠 한 번 더 해야하는 곳
-    //생각1) 특징이, section은 오름차순이니깐, 첫 번째 요소를 기준으로 시작하면 될 듯?
-    //생각2) 첫 번째를 기준으로 m길이 만큼 칠하고, 남는 부분의 가장 빠른 수를 기준으로 칠하면 될듯?
-    //생각3) 칠해진것에 대한 기준은? boolean값으로 하면 될까? -> 배열을 만들어 보자
-    const newPaint = newArr(n,section);
-    let count = 0;
-    while(newPaint.includes(false)) {
-        const firstPaint = newPaint.indexOf(false);
-        for(let j = firstPaint; j <= firstPaint+(m-1); j++) {
-            if(!newPaint[j]) {
-                newPaint[j] = true;
-            }
-            
-        }
-        count++;
-    }
-    return count;
-}
 
-function newArr(n, section) {
-    const newPaint = [];
-    for(let i = 1; i <= n; i++) {
-        if(section.includes(i)) {
-            newPaint.push(false);
-        } else {
-            newPaint.push(true);
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+function solution(n, m, sections) {
+    var answer = 0;
+    var painted = 0;
+    for(var section of sections) {
+        if(painted < section) {
+            answer++;
+            painted = section+m-1;
         }
     }
-    return newPaint;
+    return answer;
 }
