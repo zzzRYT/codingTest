cb081bf9db23d61ed24a38d0e579e272b98f1d1e
zzzRYT <jinjinstar3@gmail.com>
2024-11-14 12:33:11 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.js
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.js
@@ -2,8 +2,4 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
 let input = fs.readFileSync(filePath).toString();
 
-let result = 0;
-for (let i = Number(input) - 1; i > 0; i--) {
-  result += i;
-}
-console.log(result + "\n2");
+console.log(((Number(input) * (Number(input)-1))/2) + "\n2");
