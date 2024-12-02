f3b8c776827e441b1ad7bfcfa9a112540b2cdc81
zzzRYT <jinjinstar3@gmail.com>
2024-12-02 16:12:57 +0900

    [Bronze V] Title: A+B - 2, Time: 96 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
+let input = fs
+  .readFileSync(filePath, "utf8")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+console.log(input[0]+input[1]);
