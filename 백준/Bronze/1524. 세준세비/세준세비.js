81c91411f0db6092c91ae33e3061916d76629915
zzzRYT <jinjinstar3@gmail.com>
2024-12-03 13:04:04 +0900

    [Bronze I] Title: 세준세비, Time: 180 ms, Memory: 31720 KB -BaekjoonHub

--- 백준/Bronze/1524. 세준세비/세준세비.js
+++ 백준/Bronze/1524. 세준세비/세준세비.js
@@ -1,20 +1,22 @@
-const fs = require('fs');
-const file_path = process.platform === 'linux' ? 'dev/stdin' : `${__dirname}/input.txt`
-const input = fs.readFileSync(file_path).toString().trim();
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
+let [count, ...wars] = fs
+  .readFileSync(filePath, "utf8")
+  .toString()
+  .trim()
+  .split("\n\n");
 
-/**
- * @param {string} input 
- * @returns 
- */
-const solution = (input) => {
-    const [num, ...wars] = input.replace(/\r/g, '').split('\n').filter(el => el);
-    const rst_arr = [];
-    for(let i = 0; i < wars.length; i+= 3) {
-        const sejun = wars[i + 1].split(' ').map(el => Number(el));
-        const sebi = wars[i + 2].split(' ').map(el => Number(el));
-        Math.max(...sejun) >= Math.max(...sebi) ? rst_arr.push('S') : rst_arr.push('B');
-    }
-    return rst_arr.join('\n');
+for (let i = 0; i < count; i++) {
+  const input = wars[i].split("\n");
+  input.shift();
+  sPower = Math.max(...input.shift().split(" ").map(Number));
+  bPower = Math.max(...input.shift().split(" ").map(Number));
+
+  if (sPower >= bPower) {
+    console.log("S");
+  } else if (bPower > sPower) {
+    console.log("B");
+  } else {
+    console.log("C");
+  }
 }
-
-console.log(solution(input));
