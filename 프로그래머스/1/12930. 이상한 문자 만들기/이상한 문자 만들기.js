8121746d690af83990aa8d906f9a38f0ccb569fc
zzzRYT <jinjinstar3@gmail.com>
2024-12-10 14:04:26 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.32 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    return s
+        .split(" ")
+        .map((word) =>
+            word
+                .split("")
+                .map((char, index) =>
+                    index % 2 === 0 ? char.toUpperCase() : char.toLowerCase()
+                )
+                .join("") 
+        )
+        .join(" "); 
+}
