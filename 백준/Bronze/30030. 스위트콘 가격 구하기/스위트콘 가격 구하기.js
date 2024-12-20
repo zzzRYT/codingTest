6c7eb35eb2a586a156980c666c66f15f7019437b
zzzRYT <jinjinstar3@gmail.com>
2024-12-20 16:44:46 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 96 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
+let input = require("fs").readFileSync(filePath, "utf-8").trim().toString();
+
+const num = Number(input);
+console.log(num * (10 / 11));
