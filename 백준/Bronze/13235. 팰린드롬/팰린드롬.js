03e18720fde602adb94ddba767e99bfbadb55d4a
zzzRYT <jinjinstar3@gmail.com>
2025-01-11 16:51:58 +0900

    [Bronze II] Title: 팰린드롬, Time: 96 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/13235. 팰린드롬/팰린드롬.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
+let input = require("fs").readFileSync(filePath, "utf-8").trim().toString();
+
+const base = input.split("");
+const ans = isSame(base);
+console.log(ans);
+
+function isSame(arr) {
+  for (let i = 0; i < arr.length / 2; i++) {
+    if (arr[i] !== arr[arr.length - 1 - i]) {
+      return "false";
+    }
+  }
+  return "true";
+}
