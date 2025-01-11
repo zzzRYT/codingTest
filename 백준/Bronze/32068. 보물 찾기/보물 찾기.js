c777984c19a043c7b240d515fb090449b7fbf29d
zzzRYT <jinjinstar3@gmail.com>
2025-01-11 16:26:47 +0900

    [Bronze III] Title: 보물 찾기, Time: 236 ms, Memory: 14548 KB, Score: 48 point -BaekjoonHub

--- 백준/Bronze/32068. 보물 찾기/보물 찾기.js
+++ 백준/Bronze/32068. 보물 찾기/보물 찾기.js
@@ -10,22 +10,18 @@ const T = Number(input.shift());
 const ans = [];
 for (let i = 0; i < T; i++) {
   const [L, R, S] = input.shift().split(" ").map(Number);
-  const newArr = Array.from({ length: R - L + 1 }, (_, idx) => L + idx);
   let count = 1;
-  let now = newArr.indexOf(S);
-  let state = 0;
-  while (state < 1) {
-    if (newArr[now] === L || newArr[now] === R) {
-      state++;
-    }
+  let now = S;
+  while (true) {
     now += count;
     if (count < 0) {
       count = -1 * (count - 1);
     } else {
       count = -1 * (count + 1);
     }
+    if (now === L || now === R) break;
   }
-  ans.push(Math.abs(count) - 1);
+  ans.push(Math.abs(count));
 }
 
 console.log(ans.join("\n"));
