8daa6c7014f5afa7c5c6dd40f7f3ef39d47b905d
zzzRYT <jinjinstar3@gmail.com>
2025-01-20 10:20:44 +0900

    [Bronze II] Title: SciComLove (2022), Time: 96 ms, Memory: 9328 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/24937. SciComLove （2022）/SciComLove （2022）.js
+++ 백준/Bronze/24937. SciComLove （2022）/SciComLove （2022）.js
@@ -2,13 +2,24 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
 let input = require("fs").readFileSync(filePath, "utf-8").trim().toString();
 
-const count = Number(input);
-let cuteBabyLeaf = "SciComLove";
+const obj = {
+  1: "ciComLoveS",
+  2: "iComLoveSc",
+  3: "ComLoveSci",
+  4: "omLoveSciC",
+  5: "mLoveSciCo",
+  6: "LoveSciCom",
+  7: "oveSciComL",
+  8: "veSciComLo",
+  9: "eSciComLov",
+  0: "SciComLove",
+};
 
-for (let i = 0; i < count; i++) {
-  const firstWord = cuteBabyLeaf[0];
-  cuteBabyLeaf = cuteBabyLeaf.slice(1, cuteBabyLeaf.length);
-  cuteBabyLeaf = cuteBabyLeaf + firstWord;
+const temp = oneLocation(input);
+console.log(obj[temp]);
+
+function oneLocation(count) {
+  const countArr = count.split("");
+  const temp = countArr[countArr.length - 1];
+  return Number(temp);
 }
-
-console.log(cuteBabyLeaf);
