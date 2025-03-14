ca4dc9d762c9c39f5fc5980b1e8803580c9dcaaa
zzzRYT <jinjinstar3@gmail.com>
2025-03-14 15:46:06 +0900

    [Silver III] Title: 계단 오르기, Time: 96 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.js
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.js
@@ -1,27 +1,30 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
-let [count, ...arr] = require("fs")
+let input = require("fs")
   .readFileSync(filePath, "utf-8")
   .trim()
   .toString()
   .split("\n");
 
-const stair = [0, ...arr.map(Number)];
-const c = Number(count);
-if (c === 1) {
-  console.log(stair[1]);
-  return;
-}
-const dp = Array.from({ length: c + 1 }, () => Array(2).fill(0));
-dp[1][0] = stair[1];
-dp[1][1] = stair[1];
-if (c > 1) {
-  dp[2][0] = dp[1][1] + stair[2];
-  dp[2][1] = stair[2];
-}
-for (let i = 3; i <= c; i++) {
-  dp[i][0] = stair[i] + dp[i - 1][1];
-  dp[i][1] = stair[i] + Math.max(dp[i - 2][0], dp[i - 2][1]);
+const count = Number(input.shift());
+const arr = new Array(count);
+arr[0] = 0;
+for (let i = 1; i <= count; i++) {
+  arr[i] = Number(input[i - 1]);
 }
 
-console.log(Math.max(dp[dp.length - 1][0], dp[dp.length - 1][1]));
+const dp = Array.from({ length: count + 1 });
+
+dp[1] = arr[1];
+dp[2] = dp[1] + arr[2];
+dp[3] = Math.max(arr[1], arr[2]) + arr[3];
+
+//마지막 직전 계단을 밟은 경우
+//d[n] = dp[n-3] + arr[n-1] + arr[n];
+//마지막 2번 째 전 계단을 밟은 경우
+//dp[n] = dp[n-2] + arr[n];
+for (let i = 4; i <= count; i++) {
+  dp[i] = Math.max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i]);
+}
+
+console.log(dp[count]);
