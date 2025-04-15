dd7b0d416cb28023ab31ea538b80ff9bf11d8d6e
zzzRYT <jinjinstar3@gmail.com>
2025-04-15 17:06:00 +0900

    [Silver IV] Title: 수 찾기, Time: 300 ms, Memory: 41020 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -6,26 +6,32 @@ let input = require("fs")
   .toString()
   .split("\n");
 
-const ALength = Number(input.shift());
-const obj = {};
-const A = input
-  .shift()
+const N = Number(input[0]);
+const A = input[1]
   .split(" ")
   .map(Number)
-  .forEach((ele) => {
-    obj[ele] = true;
-  });
+  .sort((a, b) => a - b);
+const M = Number(input[2]);
+const B = input[3].split(" ").map(Number);
 
-const count = Number(input.shift());
-const checkArr = input.shift().split(" ").map(Number);
+function binarySearch(A, find) {
+  let lo = 0;
+  let hi = A.length - 1;
+
+  while (lo <= hi) {
+    let mid = Math.floor((lo + hi) / 2);
+    if (A[mid] === find) return 1;
+    if (A[mid] < find) lo = mid + 1;
+    else hi = mid - 1;
+  }
+
+  return 0;
+}
+
 const ans = [];
-for (let i = 0; i < count; i++) {
-  ans.push(isIncludeInArr(obj, checkArr[i]));
+for (let i = 0; i < M; i++) {
+  const findNumber = B[i];
+  ans.push(binarySearch(A, findNumber));
 }
 
 console.log(ans.join("\n"));
-
-function isIncludeInArr(obj, num) {
-  if (obj[num]) return 1;
-  return 0;
-}
