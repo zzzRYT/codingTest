1d0b0342aba8d6526ffa28853ca4b51b95ff9486
zzzRYT <jinjinstar3@gmail.com>
2025-06-03 13:20:59 +0900

    [Silver V] Title: K번째 수, Time: 4124 ms, Memory: 565044 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
+let input = fs.readFileSync(filePath, "utf8").trim().split("\n");
+
+const [N, K] = input[0].split(" ").map(Number);
+const arr = input[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+
+console.log(arr[K - 1]);
