63db28a805619dbcac7332fe7d6936fa7033f519
zzzRYT <jinjinstar3@gmail.com>
2025-06-23 13:54:53 +0900

    [Silver III] Title: 주유소, Time: 268 ms, Memory: 44800 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -3,11 +3,11 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
 let input = fs.readFileSync(filePath, "utf-8").trim().split("\n");
 
 const N = +input[0];
-const paths = input[1].split(" ").map(Number);
-const bases = input[2].split(" ").map(Number);
+const paths = input[1].split(" ").map((ele) => BigInt(ele));
+const bases = input[2].split(" ").map((ele) => BigInt(ele));
 
 let cur = bases[0];
-let cost = 0;
+let cost = 0n;
 
 for (let i = 0; i < N - 1; i++) {
   cost += paths[i] * cur;
@@ -16,4 +16,4 @@ for (let i = 0; i < N - 1; i++) {
   }
 }
 
-console.log(cost);
+console.log(cost.toString());
